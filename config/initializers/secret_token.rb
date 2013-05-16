e58e1fdaacfcf2a1fde591dfe5cfd84fa1f6c027
kaspernj <k@spernj.org>
2013-05-16 10:15:21 +0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubOrgReportsWebInterface::Application.config.secret_token = '5ceff669254d9d4e0468b7be4fdcf5da407ab9af5c7c2875aecc1b89587d9b05c61d3bd33de64957a34d3f62f3bc1b3c157cffdfff0f9c37392b033e5f085253'
